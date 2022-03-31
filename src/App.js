5ffdafe0bc4dc737c4480d06fcc460eded1f1a2f
faiyaj <faiyajz007@gmail.com>
2022-03-31 08:54:07 +0600

    css added globally, component created, routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "./routes/Home";
 
 function App() {
   return (
-    <div className="App">
+    <>
+        <Routes>
+            <Route path='/' element={<Home />}></Route>
+        </Routes>
 
-    </div>
+    </>
   );
 }
 
